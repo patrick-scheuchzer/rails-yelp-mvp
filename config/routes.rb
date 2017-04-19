129d03fe9af64c8466c61614d4a2575d1d835f85
Patrick Scheuchzer <scheuchzer.patrick@gmail.com>
2017-04-19 19:42:33 +0200

    removed unnecessary code

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root to: "restaurants#index"
+
   resources :restaurants do
-    resources :reviews, only: [ :new, :create ]
+    resources :reviews, only: [ :create ]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
